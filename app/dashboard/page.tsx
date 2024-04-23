23853326d4a87cf8e3671f5b2e4c78b7e27e3523
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-04-24 06:26:33 +0700

    aa2

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -15,9 +15,8 @@ import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
 require("dotenv").config();
 
-const supabaseUrl = "https://sqgifjezpzxplyvrrtev.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxZ2lmamV6cHp4cGx5dnJydGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMzNDc2NzQsImV4cCI6MjAyODkyMzY3NH0.2yYEUffqta76luZ5mUF0pwgWNx3iEonvmxxr1KJge68";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
