ef271c803f56b2d88b8844558cc6f4e808a97e97
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-04-24 06:29:07 +0700

    aaaaa

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -18,8 +18,6 @@ require("dotenv").config();
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 interface Item {
   id: number;
   name: string;
