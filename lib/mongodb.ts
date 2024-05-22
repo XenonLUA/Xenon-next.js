456aab65ebc12df6786c43f34e56743793621f56
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-05-23 01:34:36 +0700

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,19 +1,19 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_URI!, {
-	// Hapus opsi yang tidak dikenali
-	// useNewUrlParser: true,
-	// useUnifiedTopology: true,
-});
+const uri = process.env.MONGODB_URI!;
+const options = {};
 
+let client;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === 'development') {
 	if (!global._mongoClientPromise) {
+		client = new MongoClient(uri, options);
 		global._mongoClientPromise = client.connect();
 	}
 	clientPromise = global._mongoClientPromise;
 } else {
+	client = new MongoClient(uri, options);
 	clientPromise = client.connect();
 }
 
