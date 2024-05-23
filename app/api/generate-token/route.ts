2e81e99ca1025827729be3ef97eb5b0992ef3b11
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-05-24 00:29:32 +0700

    adsdasdasdas

--- app/api/generate-token/route.ts
+++ app/api/generate-token/route.ts
@@ -1,7 +1,13 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { v4 as uuidv4 } from "uuid";
+// /app/api/generate-token/route.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-	const token = uuidv4();
-	res.status(200).json({ token });
+	if (req.method === 'GET') {
+		const token = uuidv4();
+		res.status(200).json({ token });
+	} else {
+		res.setHeader('Allow', ['GET']);
+		res.status(405).end(`Method ${req.method} Not Allowed`);
+	}
 }
