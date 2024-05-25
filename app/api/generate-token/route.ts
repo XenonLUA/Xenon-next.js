b60dc85e24938499e2402d3a88942bfab3a0a54e
Xenon <49691279+XenonLUA@users.noreply.github.com>
2024-05-25 21:52:51 +0700

    tokens agains

--- app/api/generate-token/route.ts
+++ app/api/generate-token/route.ts
@@ -1,9 +1,18 @@
 // /app/api/generate-token/route.ts
 import { NextRequest, NextResponse } from 'next/server';
-import { v4 as uuidv4 } from 'uuid';
+
+const generateRandomToken = (length: number) => {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let token = '';
+	for (let i = 0; i < length; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		token += characters[randomIndex];
+	}
+	return token;
+};
 
 export async function GET(req: NextRequest) {
-	const token = uuidv4();
+	const token = generateRandomToken(32); // Generate a 32-character random token
 	console.log('Generated token:', token);  // Add this line for debugging
 	return NextResponse.json({ token });
 }
